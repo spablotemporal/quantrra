52137682a17ad039ac70bf85d0e78b50a5fca1a7
Jose Pablo Gomez <jpgo@ucdavis.edu>
2022-05-08 19:49:08 -0500

    updated structure for shiny app

---
+++ R/runQuantRRA.R
@@ -0,0 +1,9 @@
+#' @export
+runExample <- function() {
+  appDir <- system.file("shiny", "QuantRRA", package = "QuantRRA")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+  }
+  
+  shiny::runApp(appDir, display.mode = "normal")
+}
