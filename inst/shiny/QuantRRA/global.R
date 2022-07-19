ed4b683eb1ff843b2ab840685a880ede7cb50296
Jose Pablo Gomez <jpgo@ucdavis.edu>
2022-07-18 20:18:56 -0700

    splitted shiny app in multiple documents

---
+++ inst/shiny/QuantRRA/global.R
@@ -0,0 +1,28 @@
+#' Function to Call the shinny app
+#' 
+#' @export
+
+# QuantRRA: QUANTITATIVE RAPID RISK ASSESMENT 
+
+# TO DO: ------------------
+# - [x] Modify all the model files for the new specification of Pert distribution (mode, min, max) -> (min, mode, max)
+# - [ ] Implement additional distributions in the documentation. Triangle, inverse gamma, uniform, etc 
+# - [x] Tab for the sensitivity analysis
+# - [ ] Tab for the distribution fitting
+# - [ ] Tab for stratified models
+#   - [ ] Bar plot for the risk
+#   - [ ] Bubble plot
+#   - [ ] Map (option to add shp file)
+
+# Setup ----------
+# libraries used 
+library(shiny); library(shinydashboard)
+library(rlang)
+library(dplyr)
+library(plotly)
+library(visNetwork)
+library(DT)
+library(QuantRRA)
+
+init_nodes <- read.csv(unz('www/M_1.zip', 'nodes.csv'))
+init_edges <- read.csv(unz('www/M_1.zip', 'edges.csv'))
