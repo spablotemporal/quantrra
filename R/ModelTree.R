ff394adfe7f2ce0dc982193250a3e0956e5629b1
Jose Pablo Gomez <jpgo@ucdavis.edu>
2022-07-19 12:25:54 -0700

    updated dependencies and ModelTree

--- R/ModelTree.R
+++ R/ModelTree.R
@@ -4,10 +4,10 @@
 #' @export
 
 ModelTree <- function(M){
-  vs <- paste0(M$nodes$id, "[label = '", Tbl$id, '\n', M$nodes$label, "' , color = '", init_nodes$color, "']") %>% paste(., collapse = '\n ')
+  vs <- paste0(M$nodes$id, "[label = '", M$nodes$id, '\n', M$nodes$label, "' , color = '", M$nodes$color, "']") %>% paste(., collapse = '\n ')
   es <- paste0(M$edges$from, ' -> ', M$edges$to, ';\n ') %>% paste(., collapse = '\n ')
   
-  grViz(diagram = paste0(
+  DiagrammeR::grViz(diagram = paste0(
     "digraph flowchart {
     node [fontname = arial, shape = rectangle]\n ",
     vs,
