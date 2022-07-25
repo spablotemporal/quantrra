c88588996cb179e9cf712b10761d8f79839a4e79
Jose Pablo Gomez <jpgo@ucdavis.edu>
2022-07-25 14:35:24 -0700

    Added ranking plot fuction

---
+++ R/RankingPlot.R
@@ -0,0 +1,24 @@
+#' Function to plot the ranking
+#' 
+#' @param M Model outputs
+#' @param var variable to plot
+#' @param id Name of IDs
+#' @param col color of the plot
+#' @export
+#' 
+RankingPlot <- function(d, var, id = 'IDs', col = 'red4'){
+  # Ranking plot
+  x <- pull(d, 'IDs')
+  y <- pull(d, paste0(var, '_m'))
+  lb <- pull(d, paste0(var, '_q05'))
+  ub <- pull(d, paste0(var, '_q95'))
+  # lb <- rabRR$
+  plot_ly(x = x,
+          y = y, type = 'bar', color = I(col), 
+          error_y = ~list(symmetric = F,
+                          arrayminus = lb,
+                          array = ub,
+                          color = '#000000')
+  ) %>% 
+    layout(xaxis = list(categoryorder = "total descending"))
+}
