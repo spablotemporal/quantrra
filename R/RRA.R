e5a1fefac8b1c28273c8e7666564d9aaab2836d7
Jose Pablo Gomez <josepablogomez@Pablos-MacBook-Pro.local>
2022-11-01 18:31:00 -0700

    Updated RRA for some error messages and rowwise op

--- R/RRA.R
+++ R/RRA.R
@@ -9,20 +9,27 @@ RRA <- function(M, nsim){
   # Filter only inputs
   df <- M %>% 
     filter(type == 'In')
-  # Sample inputs from distribution
-  df <- lapply(1:nrow(df), function(x){
-    x <- SampleDist(x = df[x,]$distribution, n = nsim)
-  }) %>% 
-    `names<-`(df$id) %>%  # assign names
-    do.call(cbind, .) %>%  # add all columns
-    data.frame() # format as data frame
-  # Filter only outputs
-  o <- M %>% 
-    filter(type == 'Out')
-  # calculate outputs
-  for (x in 1:nrow(o)) {
-    df <-  df %>% 
-      mutate(!!o$id[x] := !!parse_quo(o$formula[x], env = caller_env()))
+  if(nrow(df) == 0){ # Check if at least one input was provided
+    print('ERROR: At least one input must be provided')
+  }else{
+    # Sample inputs from distribution
+    df <- lapply(1:nrow(df), function(x){
+      x <- SampleDist(x = df[x,]$distribution, n = nsim)
+    }) %>% 
+      `names<-`(df$id) %>%  # assign names
+      do.call(cbind, .) %>%  # add all columns
+      data.frame() # format as data frame
+    # Filter only outputs
+    o <- M %>% 
+      filter(type == 'Out')
+    if(nrow(o) > 0){ # If at least 1 output provided calculate
+      # calculate outputs
+      for (x in 1:nrow(o)) {
+        df <-  df %>% 
+          rowwise() %>% # Make sure operations are made per row
+          mutate(!!o$id[x] := !!parse_quo(o$formula[x], env = caller_env()))
+      }
+    }
   }
-  return(df)
+  return(df) 
 }
