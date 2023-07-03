f42c9981f64927d396ac78e60ae83f1636ad829d
spablotemporal <jpgo@ucdavis.edu>
2023-07-02 18:51:16 -0700

    updated data documentation

--- R/PPA.R
+++ R/PPA.R
@@ -1,9 +1,9 @@
-#' Risk of ASF introduction via live products
+#' Model to estimate the introduction risk of ASF in the OIRSA region
 #'
-#' Model adapted from the publication by Hernandez-Ibata el. al 2017. 
+#' Model adapted from the publication by OIRSA 2020. 
 #' Report ...
 #'
-#' @format ## List with two data frames
+#' @format ## A list with two data frames
 #' A list with the nodes and edges to use by the QuantRRA library
 #' \describe{
 #'   \item{nodes}{node table}
@@ -11,4 +11,4 @@
 #'   ...
 #' }
 #' @source <https://dx.plos.org/10.1371/journal.pone.0182850>
-"ASFm"
+"PPA"
