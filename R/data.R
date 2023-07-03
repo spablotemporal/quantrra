63d961e8866da8adac3c425d95fc17d24b105d19
spablotemporal <jpgo@ucdavis.edu>
2023-07-03 11:44:34 -0700

    updated data documentation

--- R/data.R
+++ R/data.R
@@ -1,5 +1,20 @@
 #' Risk of ASF introduction via live products
 #'
+#' Model adapted from the publication by OIRSA 2020. 
+#' Report ...
+#'
+#' @format ## List with two data frames
+#' A list with the nodes and edges to use by the QuantRRA library
+#' \describe{
+#'   \item{nodes}{node table}
+#'   \item{edges}{edges}
+#'   ...
+#' }
+#' @source <https://dx.plos.org/10.1371/journal.pone.0182850>
+"OIRSA"
+
+#' Risk of ASF introduction via live products
+#'
 #' Model adapted from the publication by Hernandez-Ibata el. al 2017. 
 #' Report ...
 #'
