e5811093e15da6115217f03325b645b9e95d19f3
spablotemporal <jpgo@ucdavis.edu>
2023-07-02 18:56:22 -0700

    updated OIRSA model

--- R/OIRSA.R
+++ R/OIRSA.R
@@ -1,9 +1,9 @@
-#' Model to estimate the introduction risk of ASF in the OIRSA region
+#' Risk of ASF introduction via live products
 #'
 #' Model adapted from the publication by OIRSA 2020. 
 #' Report ...
 #'
-#' @format ## A list with two data frames
+#' @format ## List with two data frames
 #' A list with the nodes and edges to use by the QuantRRA library
 #' \describe{
 #'   \item{nodes}{node table}
@@ -11,4 +11,4 @@
 #'   ...
 #' }
 #' @source <https://dx.plos.org/10.1371/journal.pone.0182850>
-"PPA"
+"OIRSA"
