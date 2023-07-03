7619e635a4adcf5a82c42ff762d355707b8e1f21
spablotemporal <jpgo@ucdavis.edu>
2023-07-02 18:44:19 -0700

    added ORCID and description for ASFm dataset

---
+++ R/ASFm.R
@@ -0,0 +1,14 @@
+#' Risk of ASF introduction via live products
+#'
+#' Model adapted from the publication by Hernandez-Ibata el. al 2017. 
+#' Report ...
+#'
+#' @format ## `ASFm`
+#' A list with the nodes and edges to use by the QuantRRA library
+#' \describe{
+#'   \item{nodes}{node table}
+#'   \item{edges}{edges}
+#'   ...
+#' }
+#' @source <https://dx.plos.org/10.1371/journal.pone.0182850>
+"ASFm"
