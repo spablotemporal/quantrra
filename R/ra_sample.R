a7cf8f94884e9ed25fe30f2966d8c4971a10bbf4
spablotemporal <jpgo@ucdavis.edu>
2024-03-17 07:27:22 +0100

    added example for ra_sample() fun

--- R/ra_sample.R
+++ R/ra_sample.R
@@ -4,6 +4,8 @@
 #' @param n Number of observations to be sampled from the distribution
 #' @param full TRUE/FALSE. If TRUE a list with the samples, and distribution specified will be returned, Only a vector of the observations will be returned otherwise.
 #' @export
+#' @examples
+#' ra_sample(x = 'pert(0.01,  0.03, 0.64)', n = 10)
 
 ra_sample <- function(x, n, full = F){
   if(rlang::is_string(x)){ # If input is a string, convert to list
@@ -13,36 +15,27 @@ ra_sample <- function(x, n, full = F){
     # Extract parameters
     dist[['parameters']] <- sub(".*(?:\\((.*)\\)).*|.*", "\\1",x) %>% 
       stringr::str_split(string = ., pattern = ',') %>% unlist() %>% as.numeric() 
-  }else{ # else, just toupper distribution name
-    dist <- x
-    dist$distribution <- toupper(dist$distribution)
+  }else{ # else, assume is a fixed value
+    dist <- list()
+    dist[["distribution"]] <- "FIXED"
+    dist[["parameters"]] <- x
   }
   # Sample from distribution
-  if(dist$distribution == 'PERT'){
-    xi <- mc2d::rpert(n = n, min = dist$parameters[1], mode = dist$parameters[2], max = dist$parameters[3])
-  }else if(dist$distribution %in% c('BINOM', 'BINOMIAL')){
-    xi <- rbinom(n = n, size = dist$parameters[1], prob = dist$parameters[2])
-  }else if(dist$distribution %in% c('TRIANGLE', 'TRI', 'TRIANG')){
-    xi <- mc2d::rtriang(n = n, min = dist$parameters[1], mode = dist$parameters[2], max = dist$parameters[3])
-  }else if(dist$distribution %in% c('POISSON', 'POI')){
-    xi <- rpois(n = n, lambda = dist$parameters[1])
-  }else if(dist$distribution %in% c('NORMAL', 'GAUSS', 'GAUSSIAN')){
-    xi <- rnorm(n = n, mean = dist$parameters[1], sd = dist$parameters[2])
-  }else if(dist$distribution %in% c('UNIFORM', 'UNIF', 'U')){
-    xi <- runif(n = n, min = dist$parameters[1], max = dist$parameters[2])
-  }else if(dist$distribution %in% c('INVERSE-GAMMA', 'INV-GAMMA', 'INVERSE GAMMA')){
-    xi <- invgamma::rinvgamma(n = n, shape = dist$parameters[1], rate =  dist$parameters[2])
-  }else if(dist$distribution %in% c('BETA')){
-    xi <- rbeta(n = n, shape1 = dist$parameters[1], shape2 = dist$parameters[2])
-  }else if(dist$distribution %in% c('MULT', 'MULTINOMIAL')){
-    xi <- one_of(n = n, x = dist$parameters)
-  }else if(dist$distribution %in% c('FIXED')){
-    xi <- rep(x = dist$parameters[1], n)
-  }else if(dist$distribution %in% c('LOG-NORMAL', 'LOGN', 'LOG NORMAL', 'LNORM', 'LNORMAL')){
-    xi <- mc2d::rlnormb(n = n, mean = dist$parameters[1], sd = dist$parameters[2])
-  }else{
-    print('Distribution not recognized/supported. Distributions supported include: Pert, Normal, Binomial, Triangle, Poisson, Uniform, and Inverse-gamma. If youre interested in support for particular distributions, please contact jpgo@ucdavis.edu')
-  }
+  xi <- switch (dist$distribution,
+                PERT = {mc2d::rpert(n = n, min = dist$parameters[1], mode = dist$parameters[2], max = dist$parameters[3])},
+                BINOM = ,BINOMIAL = {rbinom(n = n, size = dist$parameters[1], prob = dist$parameters[2])},
+                TRI =, TRIANGLE =, TRIANG = { mc2d::rtriang(n = n, min = dist$parameters[1], mode = dist$parameters[2], max = dist$parameters[3])},
+                POI =, POISSON = {rpois(n = n, lambda = dist$parameters[1])},
+                NORMAL =, N =, GAUSS =, GAUSSIAN = {rnorm(n = n, mean = dist$parameters[1], sd = dist$parameters[2])},
+                UNIFORM =, UNIF =, U = {runif(n = n, min = dist$parameters[1], max = dist$parameters[2])},
+                GAMMA = rgamma(n = n, shape = dist$parameters[1], scale = dist$parameters[2]),
+                `INVERSE-GAMMA` =, `INV-GAMMA` =, `INVERSE GAMMA` = {invgamma::rinvgamma(n = n, shape = dist$parameters[1], rate =  dist$parameters[2])},
+                BETA = {rbeta(n = n, shape1 = dist$parameters[1], shape2 = dist$parameters[2])},
+                MULT =, MULTINOMIAL = one_of(n = n, x = dist$parameters), 
+                FIXED = {rep(x = dist$parameters[1], n)},
+                `LOG_NORMAL` =, LOGN =, LNORM =, LNORMAL = {mc2d::rlnormb(n = n, mean = dist$parameters[1], sd = dist$parameters[2])},
+                "Error: Distribution not recognized/supported. Distributions supported include: Pert, Normal, Binomial, Triangle, Poisson, Uniform, and Inverse-gamma. If youre interested in support for particular distributions, please contact jpgo@ucdavis.edu"
+  )
   if(full == T){
     dist[['x']] <- xi
     return(dist)
