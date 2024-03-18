e036230d2c76d171b5768fdd2af10dbdb4741124
spablotemporal <jpgo@ucdavis.edu>
2024-03-18 21:01:42 +0100

    v0.0.2 changed the xlsx reading dynamic to use openxl instead xlsx for compatibility issues

--- R/ra_import.R
+++ R/ra_import.R
@@ -1,12 +1,29 @@
-#' Function to calculate the risk
+#' Import a model file from zip or xlsx
+#' @description
+#' A short description...
 #' 
 #' @param p Path to the model file
 #' @export
 
 
 ra_import <- function(p){
-  M <- list(nodes = read.csv(unz(p, 'nodes.csv')),
-               edges = read.csv(unz(p, 'edges.csv'))[-1,]
-               )
-  return(M)
+  # identify file type
+  ## split path string
+  ft <- sub(pattern = ".+\\.", replacement = "", x = p)
+  # depending on the file type, read it with the appropiate method
+  m <- switch (ft,
+    # read the zip file           
+    zip = "This is a zip file",
+    # read the xlsx file
+    # xlsx = "This is a xlsx file",
+    xlsx = xlsx::getSheets(p),
+    # if none of above, return error message
+    "Unsupported file type"
+  )
+  
+  # M <- list(
+  #   nodes = read.csv(unz(p, 'nodes.csv')),
+  #   edges = read.csv(unz(p, 'edges.csv'))[-1,]
+  # )
+  return(m)
 }
