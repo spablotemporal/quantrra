9f50c30ea08b3d9136fc67d4e27d408cceefcda5
spablotemporal <jpgo@ucdavis.edu>
2024-03-19 18:05:40 +0100

    updated datasets documentation

--- R/data.R
+++ R/data.R
@@ -9,7 +9,7 @@
 #'   \item{edges}{This table contains the information of the connections between the nodes of the risk assessment tree. The table is used for visualization purposes with the function quantrra::ra_plot_tree()}
 #'   ...
 #' }
-#' @source <https://www.oirsa.org/contenido/2020/AR_PPA_Edición%20revisada%2001_07_20.pdf>
+#' #' @source {OIRSA. 2020. Análisis de riesgo sobre la probabilidad de ingreso, establecimiento y diseminación de la PPA en la porcicultura de los países de la región del OIRSA.} \doi{https://www.oirsa.org/contenido/2020/AR_PPA_Edición%20revisada%2001_07_20.pdf}
 "OIRSA"
 
 #' Risk of ASF introduction via live products
@@ -23,11 +23,12 @@
 #'   \item{edges}{This table contains the information of the connections between the nodes of the risk assessment tree. The table is used for visualization purposes with the function quantrra::ra_plot_tree()}
 #'   ...
 #' }
-#' @source <https://dx.plos.org/10.1371/journal.pone.0182850>
+#' @source {Herrera-Ibatá, Diana María, Beatriz Martínez-López, Darla Quijada, Kenneth Burton, and Lina Mur. 2017. “Quantitative Approach for the Risk Assessment of African Swine Fever and Classical Swine Fever Introduction into the United States Through Legal Imports of Pigs and Swine Products.” Edited by Shuo Su. PLOS ONE 12 (8): e0182850.} \doi{https://doi.org/10.1371/journal.pone.0182850}
 "ASFm"
 
 #' Risk of ASF introduction via sub products
 #'
+#' Model used for the publication: “Quantitative Risk Assessment of African Swine Fever Introduction into Spain by Legal Import of Swine Products.” This model evaluates the risk of introduction of African Swine Fever into Spain via legal product imports.
 #'
 #' @format List with three data frames
 #' A list with the nodes, edges and stratified values to use by the quantrra library to perform risk assessment in R
@@ -37,6 +38,6 @@
 #'   \item{stratified}{This table contains the parameters used to run a stratified analysis with the function ra_run_stratified(). Each row represents one strata and the columns represent the parameters from the model for that strata.}
 #'   ...
 #' }
-#' @source <https://doi.org/10.1016/j.rvsc.2023.104990>
+#' @source {Muñoz-Pérez, Carolina, Beatriz Martínez-López, José Pablo Gómez-Vázquez, Cecilia Aguilar-Vega, Jaime Bosch, Satoshi Ito, Marta Martínez-Avilés, and José Manuel Sánchez-Vizcaíno. 2023. “Quantitative Risk Assessment of African Swine Fever Introduction into Spain by Legal Import of Swine Products.” Research in Veterinary Science, August, 104990.} \doi{https://doi.org/10.1016/j.rvsc.2023.104990}
 "asf_products"
 
