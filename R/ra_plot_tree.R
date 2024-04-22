574c11a9a4b7e8e382fe49bacf06302678e0d217
spablotemporal <jpgo@ucdavis.edu>
2024-04-21 22:18:52 -0700

    Updated model structure removing edges table and modified ra_plot_tree() to work witouth the need of the edge table

--- R/ra_plot_tree.R
+++ R/ra_plot_tree.R
@@ -4,7 +4,7 @@
 #' This function expects as an input a list object with named elements as nodes and edges, corresponding to the tables specifying the nodes of the tree and connections between them
 #' Shape and Color for the nodes are obtained directly from variables in the nodes table. If missing, default colors and shape are used
 #' 
-#' @param M Model list with nodes and edges, see example quantrra::OIRSA
+#' @param m Node table with columns id, label, type, distribution and formula see examples
 #' @param fontColor Color of the text displayed in the figure
 #' @param shape shape of the nodes, options are the same from the DiagrammeR library: ellipse, oval, diamond, egg, plaintext, point, square, triangle
 #' @return a grViz/htmlwidget figure representing the risk assessment tree
@@ -15,15 +15,15 @@
 #' 
 #' @export
 
-ra_plot_tree <- function(M, fontColor = 'black', shape = 'rectangle'){
+ra_plot_tree <- function(m, fontColor = 'black', shape = 'rectangle'){
   # Make sure a valid variables area provided
   cn <- c("id", "label", "type", "distribution", "formula")
-  if (any(!cn %in% colnames(M$nodes))){
-    stop("Provide valid variable names. Model file must include variables that specify the id, lable, type, distribution and formula see examples")
+  if (any(!cn %in% colnames(m))){
+    stop("Provide valid variable names. Model file must include variables that specify the id, label, type, distribution and formula see examples")
   }
   # If no color defined, add green for inputs, and red for outputs
-  if(is.null(M$nodes$color)){
-    M$nodes <- M$nodes %>% 
+  if(is.null(m$color)){
+    m <- m %>% 
       mutate(
         type = tolower(type),
         type = recode(type, input = "in", output = "out")
@@ -31,16 +31,39 @@ ra_plot_tree <- function(M, fontColor = 'black', shape = 'rectangle'){
       mutate(color = recode(type, out = '#FFF0F0', `in` = '#F0FFF0'))
   }
   
-  if(is.null(M$nodes$shape)){
-    M$nodes$shape <- 'rectangle'
+  if(is.null(m$shape)){
+    m$shape <- 'rectangle'
   }
   # Define nodes
   vs <- paste0(
-    M$nodes$id, 
-    "[label = '", M$nodes$id, '\n', M$nodes$label, "' , fillcolor = '", M$nodes$color, "']"
+    m$id, 
+    "[label = '", m$id, '\n', m$label, "' , fillcolor = '", m$color, "']"
   ) %>% paste(., collapse = '\n ')
   # Define edges
-  es <- paste0(M$edges$from, ' -> ', M$edges$to, ';\n ') %>% paste(., collapse = '\n ')
+  ## v0.2 -------------------
+  # In version 0.2, the edge table is generated from the nodes, no need to specify edge table
+  # First we get a list of the nodes involved in each out calculation
+  es <- m %>% 
+    mutate(type = tolower(type)) %>% 
+    filter(type %in% c("out")) %>% 
+    select(id, formula) %>% 
+    pull(formula, id) %>% 
+    lapply(., function(x){
+      strsplit(x, "[^[:alnum:]]+") %>% 
+        unlist()
+    })
+  
+  es <- lapply(1:length(es), function(n){
+    data.frame(
+      from = unlist(es[n]),
+      to = names(es)[n]
+    )
+  }) %>% 
+    do.call(rbind,.) %>% 
+    distinct() %>% 
+    filter(from %in% m$id) # make sure that use only the ids in nodes table
+  
+  es <- paste0(es$from, ' -> ', es$to, ';\n ') %>% paste(., collapse = '\n ')
   
   # Make the figure
   DiagrammeR::grViz(diagram = paste0(
